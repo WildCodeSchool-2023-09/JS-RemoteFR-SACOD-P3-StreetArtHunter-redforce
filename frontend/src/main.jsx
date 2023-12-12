70e42801eff4cbf0645bf18285214a592ae31aa5
diogo596 <diogo596@hotmail.fr>
2023-12-12 11:56:30 +0100

    Add marker on the map

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/map",
+    path: "/Map",
     element: <Map />,
   },
 ]);
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "../components/Map.scss";
 
@@ -7,12 +7,18 @@ function Map() {
   const [center] = useState({ lat: 51.505, lng: -0.09 });
   const ZOOM_LEVEL = 13;
   const mapRef = useRef(null);
+
   return (
     <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href=https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+      <Marker position={center}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 }
