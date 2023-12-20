6fb73d9497dab4e53333f9686fc24e190a0f2169
Mehdi Mehemel <mehdi.m_86@hotmail.fr>
2023-12-20 09:43:08 +0100

    Style CSS Home et inscription

--- frontend/src/pages/Inscription.jsx
+++ frontend/src/pages/Inscription.jsx
@@ -1,31 +1,33 @@
-import "../App.css";
+import "./Style.css";
 import React from "react";
 
 function Inscription() {
   return (
     <div>
-      <form className="flex flex-col my-auto">
-        <h1>Hunt for street art near you!</h1>
-        <div>
-          <label htmlFor="Username" className="text-label">
-            Username
-          </label>
-          <input type="Username" id="Username" required />
-        </div>
-        <div>
-          <label htmlFor="Email" className="text-label">
-            E-mail
-          </label>
-          <input type="email" id="email" required />
-        </div>
-        <div>
-          <label htmlFor="Password" className="password">
-            Password
-          </label>
-          <input type="password" id="password" required />
-        </div>
-        <button type="button">Sign Up</button>
-      </form>
+      <div className="form">
+        <form action="" method="post">
+          <h1>Hunt for street art near you!</h1>
+          <div>
+            <label htmlFor="Username" className="text-label">
+              Username
+            </label>
+            <input type="Username" id="Username" required />
+          </div>
+          <div>
+            <label htmlFor="email" className="text-label">
+              E-mail
+            </label>
+            <input type="email" id="email" required />
+          </div>
+          <div>
+            <label htmlFor="Password" className="password">
+              Password
+            </label>
+            <input type="password" id="password" required />
+          </div>
+          <button type="button">Sign Up</button>
+        </form>
+      </div>
     </div>
   );
 }
