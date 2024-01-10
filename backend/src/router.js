a2dc149fc7e950f7bc95d60b16264a84929b5dc0
diogo596 <diogo596@hotmail.fr>
2024-01-10 10:09:40 +0100

    create_controller_manager

--- backend/src/router.js
+++ backend/src/router.js
@@ -7,16 +7,16 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
+const artworkControllers = require("./controllers/artworkController");
 
 // Route to get a list of items
-router.get("/items", itemControllers.browse);
+router.get("/artwork", artworkControllers.browse);
 
 // Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
+router.get("/artwork/:id", artworkControllers.read);
 
 // Route to add a new item
-router.post("/items", itemControllers.add);
+router.post("/artwork", artworkControllers.add);
 
 /* ************************************************************************* */
 
