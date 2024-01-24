cc0eb0133f9c760a450d286c5259c32d9b58ddbb
Jonathan-Bernard <145134524+Jonathan-Bernard@users.noreply.github.com>
2024-01-24 11:07:07 +0100

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,6 @@ import "../css/home.css";
 
 function Home() {
   const [backgroundImageUrl, setBackgroundImageUrl] = useState("");
-
   const [login, setLogin] = useState({
     email: "johnnyboy59000@gmail.com",
     password: "admin",
@@ -37,52 +36,51 @@ function Home() {
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
-    backgroundAttachment: "fixed", // Correction : "backgroundattachment" -> "backgroundAttachment"
+    backgroundAttachment: "fixed",
     height: 844,
   };
 
   return (
     <div className="home-container" style={backgroundStyle}>
-      <img src="../public/logo-test3.png" alt="logo" className="logo" />
+      <img src="/logo-test3.png" alt="logo" className="logo" />
       <h1 className="inscription-title">Hunt for street art near you!</h1>
       <h2 className="start-text">START GAME</h2>
-        <form onSubmit={handleSubmit} className="form">
-          <div className="label-container">
-            <label htmlFor="email" className="text-label">
-              E-mail
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleChange}
-              value={login.email}
-              required
-            />
-          </div>
-          <div className="label-container">
-            <label htmlFor="password" className="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={handleChange}
-              value={login.password}
-              required
-            />
-          </div>
-          <div className="home-button-container">
-            <button className="connexion-button" type="submit">
-              Connexion
-            </button>
-            <button className="connexion-visiteur" type="button">
-              Mode visiteur
-            </button>
-          </div>
-        </form>
-      }
+      <form onSubmit={handleSubmit} className="form">
+        <div className="label-container">
+          <label htmlFor="email" className="text-label">
+            E-mail
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={handleChange}
+            value={login.email}
+            required
+          />
+        </div>
+        <div className="label-container">
+          <label htmlFor="password" className="password">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={handleChange}
+            value={login.password}
+            required
+          />
+        </div>
+        <div className="home-button-container">
+          <button className="connexion-button" type="submit">
+            Connexion
+          </button>
+          <button className="connexion-visiteur" type="button">
+            Mode visiteur
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
