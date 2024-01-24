98057396a4f0a69dbc4583bb5f89973803b88dd2
Jonathan-Bernard <johnnyboy59000@gmail.com>
2024-01-24 10:45:18 +0100

    log method

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,35 @@
 import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
 import "../css/home.css";
 
 function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [backgroundImageUrl, setBackgroundImageUrl] = useState("");
+
+  const [login, setLogin] = useState({
+    email: "johnnyboy59000@gmail.com",
+    password: "admin",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(`${import.meta.env.VITE_BACKEND_URL}/api/login`, login)
+      .then((res) => console.info(res.data))
+      .catch((err) => console.error(err));
+  };
+
+  const handleChange = (e) => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+    setIsLoggedIn(false);
+  };
+
   const getRandomImageUrl = () => {
     const randomImageIndex = Math.floor(Math.random() * 10) + 1;
     return `/background${randomImageIndex}.png`;
@@ -17,36 +44,64 @@ function Home() {
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
-    backgroundattachment: "fixed",
+    backgroundAttachment: "fixed", // Correction : "backgroundattachment" -> "backgroundAttachment"
     height: 844,
   };
+
   return (
-    <div className="home-contenair" style={backgroundStyle}>
+    <div className="home-container" style={backgroundStyle}>
       <img src="../public/logo-test3.png" alt="logo" className="logo" />
       <h1 className="inscription-title">Hunt for street art near you!</h1>
       <h2 className="start-text">START GAME</h2>
-      <form action="" method="post" className="form">
-        <div className="label-container">
-          <label htmlFor="Username" className="text-label">
-            Username/e-mail
-          </label>
-          <input type="email" id="Username" required />
-        </div>
-        <div className="label-container">
-          <label htmlFor="Password" className="password">
-            Password
-          </label>
-          <input type="password" id="password" required />
-        </div>
-        <div className="home-button-container">
-          <button className="connexion-button" type="button">
-            Connexion
-          </button>
-          <button className="connexion-visiteur" type="button">
-            Mode visiteur
+      {isLoggedIn ? (
+        <div>
+          <h1>Bienvenue, utilisateur connecté !</h1>
+          <button
+            onClick={handleLogout}
+            className="logout-button"
+            type="button"
+          >
+            Déconnexion
           </button>
         </div>
-      </form>
+      ) : (
+        <form onSubmit={handleLogin} className="form">
+          <div className="label-container">
+            <label htmlFor="email" className="text-label">
+              E-mail
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handleChange}
+              value={login.email}
+              required
+            />
+          </div>
+          <div className="label-container">
+            <label htmlFor="password" className="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleChange}
+              value={login.password}
+              required
+            />
+          </div>
+          <div className="home-button-container">
+            <button className="connexion-button" type="submit">
+              Connexion
+            </button>
+            <button className="connexion-visiteur" type="button">
+              Mode visiteur
+            </button>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
