b4713166506e49ff83de03866eac6eeb438d76a8
Anthony Gorski <38192276+GorskiAnthony@users.noreply.github.com>
2024-02-08 14:46:35 +0100

    Update schema.sql

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -6,29 +6,6 @@ CREATE TABLE `artwork` (
   `first_post_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE = InnoDB;
 
-CREATE TABLE `photos` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `photo_src` VARCHAR(200) NULL,
-  `post_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `validation_status` TINYINT NOT NULL,
-  `users_id` INT NOT NULL,
-  `artwork_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `users_id`, `artwork_id`),
-  INDEX `fk_photos_users_idx` (`users_id` ASC) VISIBLE,
-  INDEX `fk_photos_artwork1_idx` (`artwork_id` ASC) VISIBLE,
-  CONSTRAINT `fk_photos_users`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `mydb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_photos_artwork1`
-    FOREIGN KEY (`artwork_id`)
-    REFERENCES `mydb`.`artwork` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
 CREATE TABLE `users` (
   `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   `pseudo` VARCHAR(45) NOT NULL,
@@ -39,3 +16,26 @@ CREATE TABLE `users` (
   UNIQUE INDEX `pseudo_UNIQUE` (`pseudo` ASC) VISIBLE,
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE
 ) ENGINE = InnoDB;
+
+CREATE TABLE `photos` (
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `photo_src` VARCHAR(200) NULL,
+  `post_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `validation_status` TINYINT NOT NULL,
+  `users_id` INT NOT NULL,
+  `artwork_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_photos_users_idx` (`users_id` ASC) VISIBLE,
+  INDEX `fk_photos_artwork1_idx` (`artwork_id` ASC) VISIBLE,
+  CONSTRAINT `fk_photos_users`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_photos_artwork1`
+    FOREIGN KEY (`artwork_id`)
+    REFERENCES `artwork` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
